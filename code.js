cbb8934656d586f30a74070925a417a998b65f16
Gary <glswim@gmail.com>
2023-04-05 22:32:24 +0800

    Updated

--- code.js
+++ code.js
@@ -1,25 +1,23 @@
 const playerInput = document.querySelectorAll('.playerInput');
+const displayBox = document.querySelector('#displayBox');
 let typeOfGame;
-const winningPattern = [
-    //Base on Indexes
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8], 
-    [0,4,8],
-    [2,4,6]];
 
+var round = 1;
 var turn = 'player1';
 var board = ['','','','','','','','',''];
 var gameOver = false;
 var numTurn = 0; //To check for draws
 
-const playerFactory = (name, symbol, isComputer=false, isWinner = false) =>{
+const playerFactory = (name, symbol, isComputer=false) =>{
    
-    return{name, symbol, isComputer, moves:[], isWinner};
+    return{name, symbol, isComputer};
 }
+
+// Playing around with objects
+// var Players = {
+//     X: playerFactory(),
+//     Y: playerFactory()
+// }
 
 var player1 = playerFactory();
 var player2 = playerFactory();
@@ -36,46 +34,34 @@ function clearBoard(){
     });
    clearWindow();
    removeDrawingBoard();
-   numTurn = 0; //resets the number of moves
+   round = 1; //resets the number of moves
+   displayBox.innerHTML = `It's ${getPlayerTurn().name}'s turn`;
 }
 function clearWindow(){
-   const result = document.getElementById('result');
-   result.removeChild(result.children[0]);
    const delMenu = document.querySelector('.newGame-menu');
    delMenu.remove();
 }
 function newGameMenu(){
-   var menu = document.createElement('div'); //Creates a display menu
-   var h2 = document.createElement('h2'); //Creates a h2 heading
-   var container = document.createElement('div');
+   var menuContainer = document.createElement('div'); //Creates a display menu
    var againBtn = document.createElement('button'); //Creates a button for 'Play again'
    var menuBtn = document.createElement('button'); //Creates a button to go back to 'Menu'
 
-   menu.classList.add('newGame-menu'); //Adds style to menu
+   menuContainer.classList.add('newGame-menu'); //Adds style to menu
    //Adds style to button container
-   var containerClassList = ['d-flex', 'gap-3']; 
-   containerClassList.forEach(item => container.classList.add(item));
+   
 
-   h2.textContent = 'New Game';
-
-   var againBtnClassList = ['btn', 'btn-primary'];
-   againBtnClassList.forEach(item => againBtn.classList.add(item));
+   againBtn.classList.add('btn-secondary');
    againBtn.setAttribute('id','newGame');
-   againBtn.textContent='Play again';
+   againBtn.textContent='Again?';
 
-   menu.appendChild(h2);
-   container.appendChild(againBtn);
+   menuContainer.appendChild(againBtn);
 
-   var menuBtnClassList = ['btn', 'btn-success'];
-   menuBtnClassList.forEach(item => menuBtn.classList.add(item));
+   menuBtn.classList.add('btn-secondary');
    menuBtn.setAttribute('id','menu-btn');
    menuBtn.textContent='Menu';
+   menuContainer.appendChild(menuBtn);
 
-   menu.appendChild(h2);
-   container.appendChild(menuBtn);
-   menu.append(container);
-
-   document.querySelector('.window').appendChild(menu);
+   document.querySelector('.window').appendChild(menuContainer);
 
    var playAgain = document.getElementById('newGame');
    playAgain.addEventListener('click', clearBoard); //Fix this code so that when it is drawn, it deletes the button
@@ -86,109 +72,126 @@ function newGameMenu(){
 
 function backMenu(){
     clearBoard();
-    boardGame.classList.add('hidden');
-    displayName.classList.add('hidden');
-    menu.classList.remove('hidden');
+    boardGame.classList.add('visually-hidden');
+    displayName.classList.add('visually-hidden');
+    menu.classList.remove('visually-hidden');
 }
 
 function result(name){
-   const getContainer = document.getElementById('result');
-   const h3 = document.createElement('h3');
-   arr = ['text-center','fs-3','fw-semibold', 'text-primary'];
-   arr.forEach(item =>{
-       h3.classList.add(item);
-   })
    if (name ==='no winners'){
-    h3.textContent = "It's a draw";
+    return "It's a draw";
    }
    else{
-    h3.textContent = `The winner is ${name}`;
+    return `The winner is ${name}`;
    }
-   getContainer.appendChild(h3);
+   
+}
+function move(index, player){
+    board[index] = player.symbol; //Adds the move to the board
+    getSquares[index].innerHTML = player.symbol; //Displays where the player move on the board
+    if (winning(board, player.symbol) && (emptyIndexes != 0))
+    {
+        gameOver = true; //Can't make more moves after game is over
+        displayBox.innerHTML = `${player.name} is the winner`;
+        newGameMenu();
+    }
+    else if(emptyIndexes(board).length == 0)
+    {
+        gameOver = true;
+        displayBox.innerHTML = `It's a draw`;
+        newGameMenu();
+    }
+    else{
+        round++;
+        displayBox.innerHTML = `It's ${getPlayerTurn().name}'s turn`;
+    }
 }
 
 function handleSquareClick(event){
-    const square = event.target;
-    const index = square.id;
+    const index = event.target.id;
     if(board[index] === '' && !gameOver) //If this square is empty and there is no winner
     {
-        //Player vs Player
-        //Player1 makes a move
-        if (turn === 'player1') //Checks if it's player1's turn
+        move(index, getPlayerTurn());
+        if (getPlayerTurn().isComputer)
         {
-            player1.moves.push(index); 
-            numTurn++;
-            board[index]= player1.symbol; //Stores the move in the board[]
-            square.innerHTML = player1.symbol; //Draws the move to the board
-            isWinner(); //returns if the game is over
-            if(gameOver){
-                result(player1.name); //Displays the winner
-                newGameMenu(); //Displays if the player wants a rematch
-            }
-            else if(numTurn === 9)
-            {
-                result("no winners");
-                newGameMenu();
-            }
-            else{
-                if(player2.isComputer)
-                {
-                    computer();//Computer moves but doesn't check if computer has won the game or not
-                    numTurn++;
-                    isWinner();
-                    if(gameOver){
-                        result(player2.name);
-                        newGameMenu();
-                    }
-                    else if(numTurn === 9)
-                    {
-                        result("no winners");
-                        newGameMenu();
-                    }
-                    else{
-                        turn = "player1";
-                    }
-                }
-                else{
-                    turn ='player2';
-                }
-            }
+            console.log("It's the computer's turn");
+            move(computer(), getPlayerTurn());
         }
-        else{ //Player2's turn
-            player2.moves.push(index); //Player2 human makes a move
-            numTurn++;
-            board[index]= player2.symbol; //Stores the move in the board[]
-            square.innerHTML = player2.symbol; //Draws the moves on the board
-            isWinner(); //Returns if game is over or not
-            if(gameOver){
-                result(player2.name); //Displays Player2's name
-                newGameMenu();//Display if the player wants a rematch
-            }
-            else if(numTurn === 9)
-            {
-                alert("It's a draw.");
-                newGameMenu();
-            }
-            else{
-                turn ='player1'; //Back to Player 1 turn
-            }
+   
+    }
+}
+
+function winning(board, player){
+    symbol = player;
+    if (
+        (board[0] == symbol && board[1] == symbol && board[2] == symbol)|| 
+        (board[3] == symbol && board[4] == symbol && board[5] == symbol) ||
+        (board[6] == symbol && board[7] == symbol && board[8] == symbol) ||
+        (board[0] == symbol && board[3] == symbol && board[6] == symbol) ||
+        (board[1] == symbol && board[4] == symbol && board[7] == symbol) ||
+        (board[2] == symbol && board[5] == symbol && board[8] == symbol) ||
+        (board[0] == symbol && board[4] == symbol && board[8] == symbol) ||
+        (board[2] == symbol && board[4] == symbol && board[6] == symbol)
+    )
+    {
+        return true;
+    }
+    else{
+        return false;
+
+    }
+}
+
+//Returns list of the indexes of empty spots on the board
+function emptyIndexes(board){
+    let newArr =[];
+    for(let i = 0; i <board.length; i++){
+        if (board[i] === ''){
+            newArr.push(i);
         }
-       }
+    }
+    return newArr;
+}
+ let scores ={
+    X: 1,
+    O: -1,
+    tie: 0
+ };
+function minimax(newBoard, player, depth, isMaximizing){
+    let result = winning(newBoard, player);
+    if(result !== null){
+        return scores[result];
     }
 
-function isWinner(){
-    winningPattern.forEach(item=>{
-        var [a,b,c] = item;
-        //Checks the board for winners
-        //Having the winning patterns, we should check each square with each pattern
-        if (board[a] != '' && board[a] === board[b] && board[b] === board[c])
-        {
-            gameOver = true;
-            drawBoard(a,b,c);
+    if(isMaximizing){
+        let bestScore = -Infinity;
+        for (let i = 0; i < board.length - 1; i++){
+            if (board[i] == ''){
+                board[i] = player2.symbol;
+                let score = minimax(board, player2, depth + 1, false);
+                board[i] = '';
+                bestScore = max(score, bestScore);
+            }
         }
-    });
+        return bestScore;
+    }
+    else{
+        let bestScore = -Infinity;
+        for (let i = 0; i < board.length - 1; i++){
+            if (board[i] == ''){
+                board[i] = player1.symbol;
+                let score = minimax(board, player1, depth + 1, true);
+                board[i] = '';
+                bestScore = min(score, bestScore);
+            }
+        }
+        return bestScore;
+    }
+
+
 }
-var styleArr = ['bg-danger','text-light'];
+
+
 function drawBoard(a,b,c)
 {
     var square = [a,b,c];
@@ -204,38 +207,59 @@ function removeDrawingBoard(){
     elements.forEach(ele => ele.classList.remove('drawWinner'));
 }
 
-function randomNumber(){
-    const num = (board.length);
-    return Math.floor(Math.random()*num);
+function randomNumber(emptyBoard){
+    const num = (emptyBoard.length);
+    return emptyBoard[Math.floor(Math.random()*num)];
 }
 function computer(){
-    let index;
-    do{
-        index = randomNumber();
+
+    // let bestScore = -Infinity;
+
+    // let move ={};
+    let getEmptyBoard = emptyIndexes(board);
+
+    // for (let i = 0; i< board.length-1; i++)
+    // {
+    //     if (board[i] == '')
+    //     {
+    //         board[i] = ai; //player2.symbol is a computer
+    //         let score = minimax(board, 0, false);
+    //         board[i] = '';
+    //         if (score > bestScore){
+    //             bestScore = score;
+    //             move = {i}
+    //         }
+    //     }
+    // }
+
+    if (getEmptyBoard.length > 0)
+    {
+        return randomNumber(getEmptyBoard);
     }
-    while(board[index] !== '');
-    board[index] = player2.symbol;
-    player2.moves.push(index);
-    const cell = document.querySelectorAll('.square');
-    cell[index].innerHTML = player2.symbol;
+    else{
+        console.log("error");
+    }
 }
 
-playerInput.forEach(button =>{
-    button.addEventListener('click', (event)=>{
+const getPlayerTurn = () =>{
+    //If there is no winner, display the player to move first
+    //Else display winner
+    if (round % 2 != 0)
+    {
+        return player1;
+    }
+    else{
+        return player2;
+    }
+}
+function displayPlayerName(player1, player2)
+{
+    const getPlayer1Name = document.getElementById('player1_name');
+    const getPlayer2Name = document.getElementById('player2_name');
+    getPlayer1Name.innerHTML = player1.name;
+    getPlayer2Name.innerHTML = player2.name;
+}
 
-        var i = event.currentTarget.getAttribute('key');
-
-        if (i == 'o')
-        {
-             console.log('Player goes first');
-        }
-        else{
-            console.log('Computer goes first.');
-        }
-
-
-    })
-});
 
 // Menu Section 
 
@@ -279,24 +303,25 @@ function createPlayer(type){
     return player;
 }
 function openGame(event){
-    const getPlayer1Name = document.getElementById('player1_name');
-    const getPlayer2Name = document.getElementById('player2_name');
-    boardGame.classList.remove('hidden');
-    displayName.classList.remove('hidden');
-    menu.classList.add('hidden');
-    const inputName = document.querySelector('#player1Name');
+
+    boardGame.classList.remove('visually-hidden');
+    displayName.classList.remove('visually-hidden');
+    menu.classList.add('visually-hidden');
+
+    const inputName = document.querySelector('#player1');
     typeOfGame = getGametype();
+
+    //Creates Player 1 Name 
+    player1 = playerFactory(inputName.value, "O", false);
+    player2 = createPlayer(typeOfGame);
+
+    //Displays player 1 and player 2
+    displayPlayerName(player1, player2);
+    displayBox.innerHTML = `It's ${getPlayerTurn().name} turn`;
 
     getSquares.forEach(square => {
         square.addEventListener('click', handleSquareClick);
     });
-    player2 = createPlayer(typeOfGame);
-    getPlayer2Name.innerHTML = player2.name;
-
-
-    //Creates Player 1 Name 
-    player1 = playerFactory(inputName.value, "O", false);
-    //Displays player 1 and player 2
-    getPlayer1Name.innerHTML = player1.name;
-    getPlayer2Name.innerHTML = player2.name;
 }
+
+
